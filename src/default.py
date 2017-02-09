d044e31e34a05bbb7f9353ac3f882c952afa3433
Oscar Barragan <oscaribv@gmail.com>
2017-02-09 15:54:21 +0100

    Added a small manual in default.py

--- src/default.py
+++ src/default.py
@@ -1,28 +1,58 @@
-#Default commands for chidetrending
+#-----------------------------------------------#
+#                  default.py                   #
+#        Default commands for exodetrending     #
+#       Written by Oscar Barragán, Feb 2017     #
+#-----------------------------------------------#
 
+#You can change these variables here, but I recommend you
+#to do it in the input.py file
+
+#input light curve file
 lc_file='warm_jupiter.dat'
-is_seaborn = False
-method = 'substraction'
-file_type = 'V'
-toler = 2
-lsigma = 3.0
-porder = 2
-fixed_error = 1.0
-fix_error = False
-is_fix_parameters = False
+
+#File separator, default is ' ', it could be ',', '.', etc.
 file_separator = ' '
-P = 28.38229
-T0 = 7325.81705 - 4833.0
-ttran = 5.0/24.0
-td = ttran + 10./24.
 
+#Default ephemeris
+P = 28.38229               #Period (days)
+T0 = 7325.81705 - 4833.0   #T0 (days)
+ttran = 5.0/24.0           #transit duration (days)
+ttout = 10./24.            #out of transit data duration (days)
+
+#Turn it on/off to activate seaborn plots (True or False)
+is_seaborn = False
+
+#Method for the detrending (substraction or division)
+method = 'substraction'
+
+#Number minimum of data to take into account a transit 
+#for the extract_transit function (integer)
+toler = 2
+
+#How many sigma do we want for the sigma-clipping?
+lsigma = 3.0
+
+#Order of polinomial to fit
+porder = 2
+
+#If we want to fix the out put error for the light curve, set it True
+fix_error = False
+#Set the value that you want to for the error bars
+#It works only if fix_error = True
+fixed_error = 1.0
+
+#If you have a good solution for your data, you prefer to fix the parameters
+#istead of finding the best solution, set it True if the case
+is_fix_parameters = False
 #priors
-a =  4.56
-u1 = 0.5
-u2 = 0.41
-k  = 0.0172
+#If is_fix_parameters = True, these are the parameters used to perform the sigma-clipping
+#If is_fix_parameters = False, these are the priors for the curve_fit routine
+a  =  4.56    #scaled semi-major axis
+u1 = 0.5      #u1 limb darkening coefficient, Mandel & Agol, 2002
+u2 = 0.41     #u2 limb darkening coefficient, Mandel & Agol, 2002
+k  = 0.0172   #Scaled planet radius Rp/R*
 
-#prior ranges
+#if is_fix_parameters = False, these are the ranges for the parameters for curve_fit
 min_a = 2.0
 max_a = 100.0
 min_u1 = 0.0
